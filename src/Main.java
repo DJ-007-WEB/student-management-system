3221446a16e4f8e3447b274f189760886e8046ca
DJ-007-WEB <djjotwani@gmail.com>
2026-03-29 16:19:57 +0000

    Added main class with map and polymorphism

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+import java.util.*;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Codespaces setup working ✅");
+
+        Map<Integer, Student> studentMap = new HashMap<>();
+
+
+        Student s1 = new RegularStudent();
+        s1.setStudentId(1);
+        s1.setName("Steve");
+        s1.setCourse("IT");
+        s1.setBaseFee(50000);
+
+        Student s2 = new ScholarshipStudent();
+        s2.setStudentId(2);
+        s2.setName("Natasha");
+        s2.setCourse("CS");
+        s2.setBaseFee(50000);
+
+        Student s3 = new ExchangeStudent();
+        s3.setStudentId(3);
+        s3.setName("Tony");
+        s3.setCourse("AI");
+        s3.setBaseFee(50000);
+
+   
+        studentMap.put(s1.getStudentId(), s1);
+        studentMap.put(s2.getStudentId(), s2);
+        studentMap.put(s3.getStudentId(), s3);
+
+  
+        for (Student s : studentMap.values()) {
+            System.out.println("ID: " + s.getStudentId());
+            System.out.println("Name: " + s.getName());
+            System.out.println("Course: " + s.getCourse());
+            System.out.println("Final Fee: " + s.calculateFee());
+            System.out.println("------------------------");
+        }
     }
 }
